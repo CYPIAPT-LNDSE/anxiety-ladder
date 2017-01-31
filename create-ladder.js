9c0b876af33b305d30f36be94a28498e87f36b4b
WilliamSavage <williamsavage@hotmail.com>
2017-01-31 20:41:44 +0000

    changes background colour to soft blue

--- create-ladder.js
+++ create-ladder.js
@@ -1,11 +1,15 @@
+const ladder = document.getElementById('ladder');
+
 const showLadder = function(){
-  $('#ladder').addClass('opened');
-  var folds = $('.fold', '#ladder'); console.log(folds);
+  ladder.className += ' opened';
+  const folds = document.getElementsByClassName('fold'); console.log(folds);
+
+
   unfoldQ(folds,0);
 
   function unfoldQ(elem,inx){
     if(inx != elem.length){
-      $(elem[inx]).addClass('unfolded');
+      elem[inx].className += ' unfolded';
       inx = parseInt(inx)+1;
       setTimeout(function(){
         unfoldQ(elem,inx);
