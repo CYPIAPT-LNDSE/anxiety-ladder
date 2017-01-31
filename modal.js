a3d3c5674295f9b96d933c76cf39b6ca16e17ebf
WilliamSavage <williamsavage@hotmail.com>
2017-01-31 14:02:10 +0000

    renames modal id's

--- modal.js
+++ modal.js
@@ -1,14 +1,14 @@
 var modal = document.querySelector("#modal");
 var modalOverlay = document.querySelector("#modal-overlay");
 var closeButton = document.querySelector("#close-button");
-var openButton = document.querySelector("#open-button");
+var createLadder = document.querySelector("#create-ladder");
 
 closeButton.addEventListener("click", function() {
   modal.classList.toggle("closed");
   modalOverlay.classList.toggle("closed");
 });
 
-openButton.addEventListener("click", function() {
+createLadder.addEventListener("click", function() {
   modal.classList.toggle("closed");
   modalOverlay.classList.toggle("closed");
 });
