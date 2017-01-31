4a498914d362fa15cfacd8c6f098afcfe389dbe5
WilliamSavage <williamsavage@hotmail.com>
2017-01-31 20:53:00 +0000

    tidy code to remove logs, spacing and inconsistencies

--- create-ladder.js
+++ create-ladder.js
@@ -1,24 +1,20 @@
 const ladder = document.getElementById('ladder');
 
-const showLadder = function(){
+const showLadder = () => {
   ladder.className += ' opened';
-  const folds = document.getElementsByClassName('fold'); console.log(folds);
-
-
+  const folds = document.getElementsByClassName('fold');
   unfoldQ(folds,0);
+};
 
-  function unfoldQ(elem,inx){
-    if(inx != elem.length){
-      elem[inx].className += ' unfolded';
-      inx = parseInt(inx)+1;
-      setTimeout(function(){
-        unfoldQ(elem,inx);
-      }, 500);
-    }
-  };
+
+const unfoldQ = (elem,inx) =>{
+  if(inx != elem.length) {
+    elem[inx].className += ' unfolded';
+    inx = parseInt(inx)+1;
+    setTimeout(() => {
+      unfoldQ(elem,inx);
+    }, 500);
+  }
 }
-
-
-
 
 window.onload = showLadder;
