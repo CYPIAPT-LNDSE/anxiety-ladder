e7ef41b2ca3adf1f57744a11f8597a54f9baca08
jsms90 <jsms@hotmail.co.uk>
2017-02-01 17:28:36 +0000

    rename files & change links accordingly

--- modal.js
+++ modal.js
@@ -1,14 +1,14 @@
 var modal = document.querySelector("#modal");
 var modalOverlay = document.querySelector("#modal-overlay");
 var closeButton = document.querySelector("#close-button");
-var createLadder = document.querySelector("#create-ladder");
+var createCloud = document.querySelector("#create-cloud");
 
 closeButton.addEventListener("click", function() {
   modal.classList.toggle("closed");
   modalOverlay.classList.toggle("closed");
 });
 
-createLadder.addEventListener("click", function() {
+createCloud.addEventListener("click", function() {
   modal.classList.toggle("closed");
   modalOverlay.classList.toggle("closed");
 });
