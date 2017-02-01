4b9475883bb6e58a7ce96cb5ce39b9445cf823de
WilliamSavage <williamsavage@hotmail.com>
2017-02-01 16:00:45 +0000

    adds javascript for step reorder

--- create-ladder.js
+++ create-ladder.js
@@ -36,7 +36,7 @@ addStep.addEventListener('click', () => {
         <input type="range"  min="0" max="100" value="50"/>
       `
   let newStep = document.createElement('FORM');
-  newStep.className = 'challenge fold';
+  newStep.className = 'challenge fold box';
   newStep.innerHTML = step;
   ladder.appendChild(newStep);
   setTimeout(() => {
@@ -47,6 +47,9 @@ addStep.addEventListener('click', () => {
 const completeLadder = document.getElementById('completeLadder');
 
 completeLadder.addEventListener('click', ()=> {
+  //ladder first step
+  const firstStep =document.getElementById('firstStep');
+  firstStep.style.display = 'none';
   //main cloud
   const cloud = document.getElementById('cloud');
   cloud.className += ' animated bounceOutUp';
@@ -59,7 +62,62 @@ completeLadder.addEventListener('click', ()=> {
   //title
   const cloudTitle = document.getElementById('cloudTitle');
   cloudTitle.className += ' animated zoomOut'
-  ladder.className += ' animated fadeOut'
+  // ladder.className += ' animated fadeOut'
 })
+
+
+//REORDER steps
+
+
+completeLadder.addEventListener("click", layout);
+
+function layout() {
+  console.log('complete clicked');
+  var group = document.querySelector(".group");
+  var nodes = document.querySelectorAll(".box");
+  var total = nodes.length;
+  var ease  = Power1.easeInOut;
+  var boxes = [];
+
+  for (var i = 0; i < total; i++) {
+
+    var node = nodes[i];
+
+    // Initialize transforms on node
+    TweenLite.set(node, { x: 0 });
+
+    boxes[i] = {
+      transform: node._gsTransform,
+      x: node.offsetLeft,
+      y: node.offsetTop,
+      node
+    };
+  }
+  group.classList.toggle("reorder");
+  console.log('group classlist: ', group.classList);
+  console.log(group.classList);
+  for (var i = 0; i < total; i++) {
+
+    var box = boxes[i];
+
+    var lastX = box.x;
+    var lastY = box.y;
+
+    box.x = box.node.offsetLeft;
+    box.y = box.node.offsetTop;
+
+    // Continue if box hasn't moved
+    if (lastX === box.x && lastY === box.y) continue;
+
+    // Reversed delta values taking into account current transforms
+    var x = box.transform.x + lastX - box.x;
+    var y = box.transform.y + lastY - box.y;
+
+    // Tween to 0 to remove the transforms
+    TweenLite.fromTo(box.node, 0.5, { x, y }, { x: 0, y: 0, ease });
+  }
+}
+
+
 
 window.onload = showLadder, addHandlersToElements;
