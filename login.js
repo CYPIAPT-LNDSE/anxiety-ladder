bc6f48c229831cfb864e82c78641a48eaafe2f46
jsms90 <jsms@hotmail.co.uk>
2017-02-02 11:52:19 +0000

    hide form until 'let's go' disappears

--- login.js
+++ login.js
@@ -1,5 +1,9 @@
 const letsGo = document.getElementById('letsGo');
+const login = document.getElementById('login');
 
 letsGo.addEventListener('click', () => {
   letsGo.className += ' animated bounceOutDown'
+  setTimeout(function () {
+    login.className = 'login';
+  }, 500);
 })
