041ad426edac8fb1156d5cf6c195cb9f76ee709e
jsms90 <jsms@hotmail.co.uk>
2017-02-02 12:18:12 +0000

    return down animation

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const letsGo = document.getElementById('letsGo');
 const login = document.getElementById('login');
 
 letsGo.addEventListener('click', () => {
-  letsGo.className += ' animated bounceOutLeft'
+  letsGo.className += ' animated bounceOutDown'
   setTimeout(function () {
     login.style.visibility = 'visible';
   }, 500);
